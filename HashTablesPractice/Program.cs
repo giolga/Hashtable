8992edb931e6254dfc32f667a68beca32e0613bb
giolga <giolga.kumi@gmail.com>
2024-07-22 16:22:53 +0400

    first commit

---
+++ HashTablesPractice/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTablesPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable studentsTable = new Hashtable();
+
+            Student stud1 = new Student(1, "El Kumi", 99f);
+            Student stud2 = new Student(2, "Alex", 90f);
+            Student stud3 = new Student(3, "Max", 82f);
+            Student stud4 = new Student(4, "Charles", 100f);
+
+            studentsTable.Add(stud1.Id, stud1);
+            studentsTable.Add(stud2.Id, stud2);
+            studentsTable.Add(stud3.Id, stud3);
+            studentsTable.Add(stud4.Id, stud4);
+
+            Student storedStudent = (Student)studentsTable[1];
+
+            Console.WriteLine(storedStudent);
+
+            foreach (DictionaryEntry entry in studentsTable)
+            {
+                Console.WriteLine(entry.Key + " " + entry.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float GPA { get; set; }
+
+        public Student(int id, string name, float gpa)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = gpa;
+        }
+
+        public override string ToString()
+        {
+            return $"Student Id {Id}, Name {Name}, GPA {GPA}";
+        }
+    }
+}
---
+++ HashTablesPractice/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HashTablesPractice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HashTablesPractice")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b3ca470-5fdc-45b9-adc9-9d7ed89b20ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
